3afa161eebbbe9dc479a0ead725c78d0303bf234
Dylan Peppard <122654442+DylanPeppard@users.noreply.github.com>
2025-03-23 13:48:08 -0700

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,11 +5,11 @@ export function Home ({}) {
             <div className = "wrapper">
                 <div className = "left">
                     <div className = "link-wrapper">
-                        <a href="home">HOME</a>
+                        <Link to="/">HOME</Link>
                     </div>
 
                     <div className = "link-wrapper">
-                        <a href="contact">CONTACT</a>
+                        <Link to='/contact'>CONTACT</Link>
                     </div>
                 </div>
                 <div className = "my-name">
@@ -20,38 +20,38 @@ export function Home ({}) {
             <div className="portfolio-items-wrapper">
 
                 <div className="portfolio-item-wrapper">
-                    <a href="education"><img className="portfolio-item-bg" src="uci.jpg" alt="UC Irvine sign"/></a>
+                    <Link to="/education"><img className="portfolio-item-bg" src="uci.jpg" alt="UC Irvine sign"/></Link>
                     <div className="img-txt-wrapper">
                         <div className="subtitle">
-                            <a href="education">Education</a>
+                            <Link to="/education">Education</Link>
                         </div>
                     </div>
 
                 </div>
 
                 <div className="portfolio-item-wrapper">
-                    <a href="about"><img src="headshot-1.jpg" className="portfolio-item-bg" alt="Dylan Peppard"/></a>
+                        <Link to="/about"><img src="headshot-1.jpg" className="portfolio-item-bg" alt="Dylan Peppard"/></Link>
                     <div className="img-txt-wrapper">
                         <div className="subtitle">
-                            <a href="about">About me</a>
+                            <Link to="/about">About me</Link>
                         </div>
                     </div>
                 </div>
 
                 <div className="portfolio-item-wrapper">
-                    <a href="work"><img src="uscg.jpg" className="portfolio-item-bg" alt="U.S. Coast Guard logo"/></a>
+                    <Link to="/work"><img src="uscg.jpg" className="portfolio-item-bg" alt="U.S. Coast Guard logo"/></Link>
                     <div className="img-txt-wrapper">
                         <div className="subtitle">
-                            <a href="work">Work</a>
+                            <Link to="/work">Work</Link>
                         </div>
                     </div>
                 </div>
 
                 <div className="portfolio-item-wrapper">
-                    <a href="projects"><img src="psycheMenu.png" className="portfolio-item-bg" alt="Python shell program"/></a>
+                    <Link to="/projects"><img src="psycheMenu.png" className="portfolio-item-bg" alt="Python shell program"/></Link>
                     <div className="img-txt-wrapper">
                         <div className="subtitle">
-                            <a href="projects">Projects</a>
+                            <Link to="/projects">Projects</Link>
                         </div>
                     </div>
                 </div>
