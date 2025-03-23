cf0d899d529bbfe4d9e1306a3b9da82ad8a51d0e
Dylan Peppard <122654442+DylanPeppard@users.noreply.github.com>
2025-03-23 13:31:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://dylanpeppard.github.io/Portfolio/",
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true,
-  }
+  base: "/Portfolio/",
 })
